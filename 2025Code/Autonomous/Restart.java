aa12934210f87360acb53cc56c80b88fedd2cd5b
KadeDr <120338344+KadeDr@users.noreply.github.com>
2025-01-23 18:57:16 -0600

    Update Restart.java

--- 2025Code/Autonomous/Restart.java
+++ 2025Code/Autonomous/Restart.java
@@ -1,1 +1,21 @@
 package frc.robot.autonomous;
+
+public class Restart {
+
+  public void CoralIntake() {
+    // Starts auto at coral intake
+  }
+
+  public void CoralOutake() {
+    // Starts auto at coral outake
+  }
+
+  public void AlgaeIntake() {
+    // Starts auto at algae intake
+  }
+
+  public void AlgaeOutake() {
+    // Starts auto at algae outake
+  }
+  
+}
