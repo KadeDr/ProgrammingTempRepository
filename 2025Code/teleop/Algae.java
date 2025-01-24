16368ff23f0b9910dbf5d839278f9f62f51fc04b
KadeDr <120338344+KadeDr@users.noreply.github.com>
2025-01-23 18:29:53 -0600

    Update and rename algae.java to Algae.java

---
+++ 2025Code/teleop/Algae.java
@@ -0,0 +1,18 @@
+package frc.robot.teleop;
+
+public class Algae {
+
+  // Variables
+    /*
+    Motor Controllers
+    */
+
+  public void Intake() {
+    // Spins the motors in the negative direction to intake the ball
+  }
+
+  public void Outake() {
+    // Spins the motors in the positive direction to outake the ball
+  }
+  
+}
