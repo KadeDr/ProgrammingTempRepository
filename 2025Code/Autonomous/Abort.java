ec61f18263e3cfa3b84fe507deedf42e083095a6
thatrandomdev <91132987+thatrandomdev@users.noreply.github.com>
2025-01-28 17:11:05 -0600

    Update and rename Abort.java to Autonomous.java

--- 2025Code/Autonomous/Abort.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.autonomous;
-
-public class Abort {
-
-  public void AbortAuto() {
-    // Aborts any running auto
-  }
-  
-}
---
+++ 2025Code/Autonomous/Autonomous.java
@@ -0,0 +1,11 @@
+package frc.robot.autonomous;
+
+public class Autonomous {
+
+  boolean autoEnabled;
+
+  public void AbortAuto() {
+    autoEnabled = false; // Aborts any running auto
+  }
+  
+}
